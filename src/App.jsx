ec588a00fa9a39d9eb1d2a180ee8451be56efc3b
codeacme17 <vir_kings@qq.com>
2022-10-23 22:57:52 +0800

    add tailwind @2022.10.23

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import Home from "./components/Home";
 
 export default function App() {
+  if (
+    localStorage.theme === "dark" ||
+    (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+  localStorage.theme = "light";
+  localStorage.theme = "dark";
+  localStorage.removeItem("theme");
+
   return (
-    <div className="App">
-      <Home />
+    <div className="App bg-dark-100 h-[100vh]">
+      <Home />˝
     </div>
   );
 }
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,7 +1,30 @@
 import React, { Component } from "react";
+import SearchIcon from "../../icons/search";
 
 export default class Home extends Component {
+  TAG = "codeacme17";
+
   render() {
-    return <div>Home</div>;
+    return (
+      <section className="w-full">
+        {/* Search Bar Container */}
+        <section className="pt-[30%] text-center">
+          {/* TAG */}
+          <div className="text-4xl text-dark-200 font-black">{this.TAG.toUpperCase()}</div>
+
+          {/* INPUT */}
+          <label className="relative">
+            <span className="absolute inset-y-0 left-2 flex items-center pl-2">
+              <SearchIcon />
+            </span>
+            <input
+              type="text"
+              className="mt-10 text-dark-300 bg-dark-200 outline-none rounded-3xl w-[560px] h-10 px-12 focus:rounded-md ease-in-out duration-300 text-md placeholder:text-dark-100 border-2 border-black dark:boder-0"
+              placeholder="Search You Want"
+            />
+          </label>
+        </section>
+      </section>
+    );
   }
 }
