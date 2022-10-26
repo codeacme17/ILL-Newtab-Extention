470819666edc400c0297921568c18b02d03f1434
codeacme17 <vir_kings@qq.com>
2022-10-26 21:39:31 +0800

    REMOVE TAG ABOVE SEARCH BAR @2022.10.26

--- src/components/Searchbar/index.jsx
+++ src/components/Searchbar/index.jsx
@@ -3,7 +3,6 @@ import SearchIcon from "../../icons/search";
 import BingIcon from "../../icons/bing";
 import GoogleIcon from "../../icons/google";
 import BaiduIcon from "../../icons/baidu";
-import "./index.scss";
 
 export default class Search extends Component {
   state = {
@@ -43,30 +42,25 @@ export default class Search extends Component {
   };
 
   render() {
-    const TAG = "codeacme 17";
     const { searchEngine } = this.state;
-    return (
-      <section className="w-full py-[200px] flex flex-col items-center">
-        {/* TAG */}
-        <div className="text-4xl text-dark-200 font-black dark:text-main-400">
-          {TAG.toUpperCase()}
-        </div>
 
+    return (
+      <section className="w-full py-[200px] flex justify-center">
         {/* INPUT */}
-        <label className="input_label relative mt-10">
-          <span className="absolute top-2 left-3.5">
+        <label className="relative">
+          <span className="absolute top-3 left-3.5">
             <SearchIcon />
           </span>
 
           <input
             type="text"
-            className="input_bar w-[560px] h-10 pl-12 pr-32 text-dark-300 outline-none rounded-3xl ease-in-out duration-200 dark:text-main-400 border-[1px] border-black shadow-inner dark:border-main-500 dark:bg-main-800 placeholder:text-sm placeholder:leading-12 placeholder:text-dark-100 dark:placeholder:text-main-600 focus:rounded-md"
+            className="w-[660px] h-12 pl-12 pr-32 text-dark-300 outline-none rounded-3xl ease-in-out duration-200 dark:text-main-400 border-[1px] border-black shadow-inner dark:border-main-500 dark:bg-main-800 placeholder:text-sm placeholder:leading-12 placeholder:text-dark-100 dark:placeholder:text-main-600 focus:rounded-md"
             placeholder="SEARCH YOU WANT"
             ref={this.InputRef}
             onKeyUp={this.search}
           />
 
-          <div className="engine_btns absolute top-2 right-3 flex items-center ease-in-out duration-300 transition-[fill,opacity]">
+          <div className="absolute top-3 right-3 flex items-center ease-in-out duration-300 transition-[fill,opacity]">
             <button
               className={`mr-2 ${
                 searchEngine === this.BING_URL
